122122dbb65c137301001ea48bac32f72f1d0be2
BlastermanR <112714065+BlastermanR@users.noreply.github.com>
2025-03-29 17:26:46 -0400

    maze stuff

--- src/robot.cpp
+++ src/robot.cpp
@@ -6,17 +6,52 @@ Robot::Robot()
     return;
 }
 
-void Robot::move_forward(float distance_mm, CorrectionTypes correction, bool block)
+// Instructs main process to move forward, by how much and if correcitons/blocking are used
+void Robot::move_forward(float distance_mm, bool block, CorrectionTypes correction)
 {
-    uint64_t intiial_rotations_left = left_motor_rotations.read();
-    uint64_t initial_rotations_right = right_motor_rotations.read();    
+    // Calculate encoder lines
+    if (motor_action.read()) {return;} // Abort if motor action in progress
+    int64_t encoder_lines = (int64_t)round(distance_mm * ENCODER_NUM_LINES_ROTATION * EXTERNAL_GEAR_RATIO / WHEEL_CIRCUMFERENCE_MM);
+    left_encoder_rotation_demand.write(encoder_lines);
+    right_encoder_rotation_demand.write(encoder_lines);
+    
+    // Set Correction
+    // TODO: CREATE SYSTEM FOR MODE SWITCHING STRIAGHT/DIAGONAL
+    if (correction == NONE) 
+    {
+        calculate_corrections.write(false);
+    }
+    else
+    {
+        calculate_corrections.write(true);
+    }
 
-    // TODO: use encoder rotation data + gyro data to judge distance
+    // Start Motor
+    motor_action.write(true);
+
+    // Nested to avoid unecessary mutex action
+    while(block && motor_action.read()) {sleep_ms(5);}
 }
 
-void Robot::turn(TurnTypes direction, bool block)
+// Instructs main process to turn, by how much and if correcitons/blocking are used
+void Robot::turn(int direction_cw, bool block)
 {
-    // TODO Complete using gyro data / encoders
+    // Calculate encoder lines
+    if (motor_action.read()) {return;} // Abort if motor action in progress
+
+    // Calculate Encoder Lines
+    int64_t encoder_lines = (int64_t)round(direction_cw / 360 * ENCODER_NUM_LINES_ROTATION * WHEEL_BASE_MM / WHEEL_DIAMETER_MM * EXTERNAL_GEAR_RATIO);
+    left_encoder_rotation_demand.write(encoder_lines);
+    right_encoder_rotation_demand.write(-1 * encoder_lines);
+
+    // Set Correction
+    calculate_corrections.write(false);
+
+    // Start Motor
+    motor_action.write(true);
+
+    // Nested to avoid unecessary mutex action
+    while(block && motor_action.read()) {sleep_ms(5);}
 }
 
 void Robot::calibrate_position()
